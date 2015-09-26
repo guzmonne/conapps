45b55a7d026c1223fd075f4d21821b043f389cf4
Guzman Monne <guzmonne@hotmail.com>
2015-09-26 08:41:25 -0300

    updated meraki estimates index and states

--- meraki-estimates/client/directives/estimates-toolbar.directive.js
+++ meraki-estimates/client/directives/estimates-toolbar.directive.js
@@ -1,7 +1,10 @@
-angular.module('conapps').directive('estimatesToolbar', function(){
+angular.module('conapps').directive('estimatesToolbar', function($location){
 	return {
 		restrict: 'E',
 		replace: true,
 		templateUrl: 'meraki-estimates/client/views/meraki-estimates-toolbar.template.ng.html',
+		link: function(scope, element){
+			element.find('a[href="'+$location.$$url+'"]').addClass('active');
+		}
 	};
 });
--- meraki-estimates/client/meraki-estimates.states.js
+++ meraki-estimates/client/meraki-estimates.states.js
@@ -14,14 +14,63 @@ angular.module("conapps").config(['$stateProvider',
           'estimates-toolbar': {
             template: '<estimates-toolbar></estimates-toolbar>'
           },
+          'estimates-workview': {
+            template: '<h1>Here goes the Index WorkView</h1>'
+          }
         },
-        /*
         resolve : {
           "currentUser": ['$meteor', function($meteor){
             return $meteor.requireUser();
           }]
         },
-        */
+      })
+      .state('meraki_estimates.edit', {
+        url: '/edit',
+        views: {
+          'estimates-toolbar': {
+            template: '<h1>Here goes the New Sidebar Toolbar</h1>'
+          },
+          'estimates-workview': {
+            template: '<h1>Here goes the New WorkView</h1>'
+          }
+        },
+        resolve : {
+          "currentUser": ['$meteor', function($meteor){
+            return $meteor.requireUser();
+          }]
+        },
+      })
+      .state('meraki_estimates.shared', {
+        url: '/shared',
+        views: {
+          'estimates-toolbar': {
+            template: '<estimates-toolbar></estimates-toolbar>'
+          },
+          'estimates-workview': {
+            template: '<h1>Here goes the Shared WorkView</h1>'
+          }
+        },
+        resolve : {
+          "currentUser": ['$meteor', function($meteor){
+            return $meteor.requireUser();
+          }]
+        },
+      })
+      .state('meraki_estimates.price_list', {
+        url: '/price_list',
+        views: {
+          'estimates-toolbar': {
+            template: '<estimates-toolbar></estimates-toolbar>'
+          },
+          'estimates-workview': {
+            template: '<h1>Here goes the PriceList WorkView</h1>'
+          }
+        },
+        resolve : {
+          "currentUser": ['$meteor', function($meteor){
+            return $meteor.requireUser();
+          }]
+        },
       });
     }
   ]);
